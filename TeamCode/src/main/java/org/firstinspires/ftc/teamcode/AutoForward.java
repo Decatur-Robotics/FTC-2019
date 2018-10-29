9ec95865796d02b153d1a08b5f31741da4e374f6
VeryCleverName3 <07jamesemr@csdecatur.net>
2018-10-29 11:46:37 -0400

    Just an auto that goes straight 90 inches from start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoForward.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//TODO: Test this entire opmode, it likely won't work great.
+
+@Autonomous(name="FacingCrate", group="Autonomous")
+
+public class AutoForward extends OpMode {
+    Hardware_4232 robot = new Hardware_4232();
+    private ElapsedTime runtime = new ElapsedTime();
+    private AutomodeFunctionality automode = new AutomodeFunctionality();
+
+
+
+    public void init(){}
+    public void init_loop(){}
+
+
+
+    @Override
+    public void start () {
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        robot.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0", "Starting at %7d :%7d", robot.leftMotor.getCurrentPosition(), robot.rightMotor.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+    }
+    @Override
+    public void loop(){
+        // move forward 90 inches.
+        automode.moveInches(90, 90, .7, 5, robot, false);
+    }
+}
